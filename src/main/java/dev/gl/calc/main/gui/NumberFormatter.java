eb1d388a11879df5a9defef18bc932673a51caf1
gl <g.lipovtsev@gmail.com>
2025-04-25 11:23:12 +0300

    fixed: added case for start of entering decimal value

---
+++ src/main/java/dev/gl/calc/main/gui/NumberFormatter.java
@@ -0,0 +1,17 @@
+package dev.gl.calc.main.gui;
+
+/**
+ *
+ * @author gl
+ */
+public class NumberFormatter {
+    public static String convertDoubleToIntegerIfNoDecimalPart(String doubleVal) {
+        // case when user started to write decimal number
+        if (doubleVal.charAt(doubleVal.length() - 1) == '.') return doubleVal;
+        
+        // all other cases
+        return Double.parseDouble(doubleVal) % 1 == 0
+                ? String.valueOf(((Double) Double.parseDouble(doubleVal)).intValue())
+                : String.valueOf(Double.parseDouble(doubleVal));
+    }
+}
