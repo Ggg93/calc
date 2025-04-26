b9e550ccdca4ebae3a465b90fd3f4468435796d9
gl <g.lipovtsev@gmail.com>
2025-04-26 11:45:36 +0300

    Number formatting is in only one class - NumberFormatter - now

--- src/main/java/dev/gl/calc/main/gui/LimitedLengthDocumentFilter.java
+++ src/main/java/dev/gl/calc/main/gui/LimitedLengthDocumentFilter.java
@@ -1,7 +1,5 @@
 package dev.gl.calc.main.gui;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
@@ -12,87 +10,56 @@ import javax.swing.text.DocumentFilter;
  * @author gl
  */
 public class LimitedLengthDocumentFilter extends DocumentFilter {
-    private static final DecimalFormat DEFAULT_FORMATTER;
-    private static final DecimalFormat SCIENTIFIC_FORMATTER;
+
     private int lengthLimit;
-    
-    static {
-        DecimalFormatSymbols symbols = DecimalFormatSymbols.getInstance();
-        symbols.setGroupingSeparator(' ');
-        DEFAULT_FORMATTER = new DecimalFormat("###,###.########", symbols);
-        SCIENTIFIC_FORMATTER = new DecimalFormat("0.###E0");
-    }
-    
+
     public LimitedLengthDocumentFilter(int lengthLimit) {
         this.lengthLimit = lengthLimit;
     }
-    
+
     private boolean isEnoughSpace(String text) {
-        return text.replaceAll(" ", "").length() <= lengthLimit;
+        return text.replaceAll("\\D", "").length() <= lengthLimit;
     }
-    
+
     @Override
     public void insertString(FilterBypass fb, int offset, String string,
             AttributeSet attr) throws BadLocationException {
-        
+
         Document doc = fb.getDocument();
         String oldText = doc.getText(0, doc.getLength());
         StringBuilder sb = new StringBuilder(oldText);
         sb.insert(offset, string);
-        
+
         if (isEnoughSpace(sb.toString())) {
             super.insertString(fb, offset, string, attr);
         }
-        
+
     }
-    
+
     @Override
     public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs)
             throws BadLocationException {
         Document doc = fb.getDocument();
         String oldText = doc.getText(0, doc.getLength());
         StringBuilder sb = new StringBuilder(oldText);
-        
+
         sb.replace(offset, offset + length, text);
-        String copy = sb.toString(); // needed in case to apply another formatter
-        boolean isTheLastCharacterADot = sb.charAt(sb.length() - 1) == '.';
-        String formatted = DEFAULT_FORMATTER.format(Double.parseDouble(sb.toString()));
-        sb = new StringBuilder(formatted);
-        
-        if (isTheLastCharacterADot) {
-            sb.append(".");
-        }
-        
-        // if the number is too small, than apply another formatter
-        if (sb.toString().equals("0") && !copy.toString().equals("0")) {
-            formatted = SCIENTIFIC_FORMATTER.format(Double.parseDouble(copy.toString()));
-            sb = new StringBuilder(formatted);
-        }
-        
         if (isEnoughSpace(sb.toString())) {
-            super.replace(fb, offset, length, sb.toString(), attrs);
-//            super.replace(fb, offset, length, text, attrs);
-        } else {
-            // if value is more then lengthLimit (see MainWindow.OPERAND_LENGTH_LIMIT),
-            // show it in scientific notation
-            String preparedString = sb.toString().replaceAll(" ", "");
-            preparedString = preparedString.replaceAll(",", ".");
-            String exponentialOutput = SCIENTIFIC_FORMATTER.format(Double.parseDouble(preparedString));
-            super.replace(fb, offset, length, exponentialOutput, attrs);
+            super.replace(fb, offset, length, text, attrs);
         }
     }
-    
+
     @Override
     public void remove(FilterBypass fb, int offset, int length) throws BadLocationException {
         Document doc = fb.getDocument();
         String oldText = doc.getText(0, doc.getLength());
         StringBuilder sb = new StringBuilder(oldText);
-        
+
         sb.replace(offset, offset + length, "");
-        
+
         if (isEnoughSpace(sb.toString())) {
-            super.remove(fb, offset, length);            
+            super.remove(fb, offset, length);
         }
     }
-    
+
 }
--- src/main/java/dev/gl/calc/main/gui/NumberFormatter.java
+++ src/main/java/dev/gl/calc/main/gui/NumberFormatter.java
@@ -1,17 +1,69 @@
 package dev.gl.calc.main.gui;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
 /**
  *
  * @author gl
  */
 public class NumberFormatter {
+
+    private static final DecimalFormat DEFAULT_FORMATTER;
+    private static final DecimalFormat SCIENTIFIC_FORMATTER;
+
+    static {
+        DecimalFormatSymbols symbols = DecimalFormatSymbols.getInstance();
+        symbols.setGroupingSeparator(' ');
+        DEFAULT_FORMATTER = new DecimalFormat("#,###.##############", symbols);
+        SCIENTIFIC_FORMATTER = new DecimalFormat("0.##########E0###");
+    }
+
     public static String convertDoubleToIntegerIfNoDecimalPart(String doubleVal) {
-        // case when user started to write decimal number
-        if (doubleVal.charAt(doubleVal.length() - 1) == '.') return doubleVal;
-        
-        // all other cases
-        return Double.parseDouble(doubleVal) % 1 == 0
-                ? String.valueOf(((Double) Double.parseDouble(doubleVal)).longValue())
-                : String.valueOf(Double.parseDouble(doubleVal));
+
+        // check for (1) last char is '.' and calculate amount of tralinig zeros
+        boolean isTheLastCharacterADot = doubleVal.charAt(doubleVal.length() - 1) == '.';
+        int trailingZeros = 0;
+        if (!isTheLastCharacterADot && doubleVal.contains(".")) {
+            char c = '0';
+            int idx = doubleVal.length() - 1;
+            while (c == '0') {
+                c = doubleVal.charAt(idx--);
+                if (c == '0') {
+                    trailingZeros++;
+                }
+            }
+        }
+
+        String formatted = DEFAULT_FORMATTER.format(Double.parseDouble(doubleVal));
+
+        StringBuilder sb = new StringBuilder(formatted);
+        if (isTheLastCharacterADot) {
+            sb.append(".");
+        } else if (trailingZeros > 0) {
+            sb.append(".");
+            while (trailingZeros-- > 0) {
+                sb.append("0");
+            }
+        }
+
+        // if the number is too small, than apply another formatter
+        if (sb.toString().equals("0") && !doubleVal.equals("0")) {
+            formatted = SCIENTIFIC_FORMATTER.format(Double.parseDouble(doubleVal));
+            sb = new StringBuilder(formatted);
+        } else {
+            // if value is more then lengthLimit (see MainWindow.OPERAND_LENGTH_LIMIT),
+            // show it in scientific notation
+            String preparedString = sb.toString().replaceAll(" ", "");
+            preparedString = preparedString.replaceAll(",", ".");
+            if (preparedString.length() > MainWindow.OPERAND_LENGTH_LIMIT) {
+                String exponentialOutput = SCIENTIFIC_FORMATTER.format(Double.parseDouble(preparedString));
+                sb = new StringBuilder(exponentialOutput);
+            }
+
+        }
+
+        return String.valueOf(sb.toString());
+
     }
 }
